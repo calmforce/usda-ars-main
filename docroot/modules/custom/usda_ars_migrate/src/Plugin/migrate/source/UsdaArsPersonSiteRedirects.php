96ec3e9a1232b00800aebd38d6d52f2744e003eb
Vladimir Ivanov <vladimir_ivanov@hcl.com>
2023-04-27 19:34:55 -0400

    UM2D9-204: person profile redirects WIP

--- docroot/modules/custom/usda_ars_migrate/src/Plugin/migrate/source/UsdaArsPersonSiteRedirects.php
+++ docroot/modules/custom/usda_ars_migrate/src/Plugin/migrate/source/UsdaArsPersonSiteRedirects.php
@@ -9,15 +9,15 @@ use Drupal\migrate\Row;
  * Defines UsdaArsPersonSitePages migrate source plugin.
  *
  * This annotation tells Drupal that the name of the MigrateSource plugin
- * implemented by this class is "usda_ars_person_site_pages". This is the name
+ * implemented by this class is "usda_ars_person_site_redirects". This is the name
  * that the migration configuration references with the source "plugin" key.
  *
  * @MigrateSource(
- *   id = "usda_ars_person_site_pages",
+ *   id = "usda_ars_person_site_redirects",
  *   source_module = "usda_ars_migrate"
  * )
  */
-class UsdaArsPersonSitePages extends UsdaArsSource {
+class UsdaArsPersonSiteRedirects extends UsdaArsSource {
 
   /**
    * {@inheritdoc}
@@ -35,19 +35,13 @@ class UsdaArsPersonSitePages extends UsdaArsSource {
     $query->innerJoin('cmsDocument', 'pd', 'pd.nodeId = pc.nodeId');
 
     $query->addField('c', 'nodeId', 'nodeId');
-    $query->addField('n', 'text', 'nodeName');
-    $query->addField('d', 'updateDate', 'updateDate');
-    $query->addField('ct', 'alias', 'node_type');
-    $query->addField('pct', 'alias', 'parent_type');
-    $query->addField('pd', 'documentUser', 'nodeUser');
-    $query->fields('n', ['sortOrder', 'createDate', 'parentID', 'level', 'path']);
     $query->condition('ct.alias', 'PersonSite');
     // Add parent condition.
     $query->condition('pct.alias', 'PeopleFolder');
     $query->condition('d.published', 1);
     // Parent has to be published as well.
     $query->condition('pd.published', 1);
-    $query->orderBy('level', 'ASC');
+    $query->orderBy('c.nodeId', 'ASC');
     return $query;
   }
 
@@ -57,11 +51,6 @@ class UsdaArsPersonSitePages extends UsdaArsSource {
   public function fields() {
     $fields = [
       'nodeId' => $this->t('Node ID'),
-      'nodeName' => $this->t('Title'),
-      'sortOrder' => $this->t('Sort Order'),
-      'createDate' => $this->t('Date Created'),
-      'updateDate' => $this->t('Date Updated'),
-      'nodeUser' => $this->t('Node User'),
     ];
     return $fields;
   }
@@ -99,34 +88,6 @@ class UsdaArsPersonSitePages extends UsdaArsSource {
 
     }
     return parent::prepareRow($row);
-  }
-
-  /**
-   * Sets person profile properties.
-   *
-   * @param object $row
-   *   The Drupal\migrate\Row object.
-   * @param object $person_properties
-   *   The fields retrieved from aris_bublic_web DB.
-   */
-  protected function setPersonSourceProperties($row, $person_properties) {
-    $mode_1 = $person_properties->mode_1;
-    $mode_2 = $person_properties->mode_2;
-    $mode_3 = $person_properties->mode_3;
-    $mode_4 = $person_properties->mode_4;
-    if ($mode_1 && $mode_2 && $mode_3 && $mode_4) {
-      $mode_code = $mode_1 . "-" . $mode_2 . "-" . $mode_3 . "-" . $mode_4;
-      $row->setSourceProperty('Mode_code', $mode_code);
-      unset($person_properties->mode_1);
-      unset($person_properties->mode_2);
-      unset($person_properties->mode_3);
-      unset($person_properties->mode_4);
-    }
-    foreach ($person_properties as $key => $value) {
-      if (!empty($value)) {
-        $row->setSourceProperty($key, $value);
-      }
-    }
   }
 
 }
